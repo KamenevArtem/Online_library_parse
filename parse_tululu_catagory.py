87ddfb7b98c868e432784fbf4a08a310e12b2fbd
KamenevArtem <112734642+KamenevArtem@users.noreply.github.com>
2023-03-20 06:20:46 +0300

    Create parse_tululu_catagory.py

---
+++ parse_tululu_catagory.py
@@ -0,0 +1,25 @@
+import requests
+import bs4
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+from urllib.parse import urlparse
+
+
+def create_book_link(url_template):
+    books_url = 'https://tululu.org/l55/'
+    parsing_response = requests.get(books_url, verify=False)
+    parsing_response.raise_for_status()
+    page_html = BeautifulSoup(parsing_response.text, 'lxml')
+    book = page_html.find('table', class_='d_book').find('td').find('a')['href']
+    book_url = urljoin(url_template, book)
+    print(book_url)
+
+
+
+def main():
+    url_template = 'https://tululu.org/'
+    create_book_link(url_template)
+
+
+if __name__ == '__main__':
+    main()
