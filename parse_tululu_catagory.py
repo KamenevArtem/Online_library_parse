cf288f3b29205cf0dbc64986aab5c2924c33dc17
KamenevArtem <112734642+KamenevArtem@users.noreply.github.com>
2023-03-21 05:21:53 +0300

    download books from all pages

--- parse_tululu_catagory.py
+++ parse_tululu_catagory.py
@@ -1,32 +1,177 @@
 import requests
-import bs4
+import pathlib
+import urllib
+import os
+import logging
+import json
+from pprint import pprint
+from pathlib import Path
+from requests import HTTPError
 from bs4 import BeautifulSoup
+from retry import retry
 from urllib.parse import urljoin
 from urllib.parse import urlparse
+from pathvalidate import sanitize_filename
 
 
-def create_book_links(page_html, url_template):
+def parse_book_ids(page_html):
+    url = 'https://tululu.org/'
     books = page_html.find_all('table', class_='d_book')
-    book_url = [urljoin(url_template, book.find('td').find('a')['href'])
+    book_urls = [urljoin(url, book.find('td').find('a')['href'])
                 for book in books
                 ]
-    print(book_url)
+    book_ids = [book_url.split('b')[1] for book_url in book_urls]
+    return book_ids
 
 
-def parse_pages(pages_quantity, url_template):
+def parse_pages(pages_quantity):
     page_url_template = 'https://tululu.org/l55/{}'
+
+    book_ids = []
     for page_number in range(1, pages_quantity+1):
         page_url = page_url_template.format(page_number)
         parsing_response = requests.get(page_url, verify=False)
         parsing_response.raise_for_status()
         page_html = BeautifulSoup(parsing_response.text, 'lxml')
-        create_book_links(page_html, url_template)
+        book_ids.append(parse_book_ids(page_html))
+    flat_list_of_ids = [book_id for page_ids in book_ids for book_id in page_ids]
+    return flat_list_of_ids
+
+
+def define_extension(file_url):
+    parsed_url = urlparse(file_url)
+    parsed_path = parsed_url.path
+    parsed_path = urllib.parse.unquote(parsed_path)
+    file_path, file_extension = os.path.splitext(parsed_path)
+    file_name = file_path.split('/')[2]
+    return file_extension, file_name
+
+
+def check_for_redirect(response):
+    if response.is_redirect:
+        raise HTTPError(response.status_code, 'Переадресация')
+
+
+@retry(TimeoutError, ConnectionError,
+       delay=1, backoff=4, max_delay=4)
+def download_txt(book_text, script_path, book_name):
+    file_path = script_path.joinpath('books')
+    file_path.mkdir(exist_ok=True)
+    file_name = sanitize_filename(f'{book_name}.txt')
+    with open(Path(file_path).joinpath(file_name),
+              'wb') as book:
+        book.write(book_text)
+
+
+@retry(TimeoutError, ConnectionError,
+       delay=1, backoff=4, max_delay=4)
+def download_image(url, script_path):
+    image_response = requests.get(
+        url,
+        verify=False
+    )
+    image_response.raise_for_status()
+    file_path = script_path.joinpath('images')
+    file_path.mkdir(exist_ok=True)
+    file_extension, image_name = define_extension(url)
+    image_name = (f'{image_name}.{file_extension}')
+    with open(Path(file_path).joinpath(image_name),
+              'wb') as image:
+        image.write(image_response.content)
+
+
+def parse_book_page(page_html, book_url, book_descriptions):
+    book_description = page_html.find('table').find('h1')
+    book_description = book_description.text.split(' \xa0 :: \xa0 ')
+    book_title, book_author = book_description
+    book_image = page_html.find(
+        'div', class_='bookimage').find('img')['src']
+    book_image_url = urljoin(book_url, book_image)
+    parsed_comments = page_html.find_all('div', class_='texts')
+    comments = [comment.find('span').text
+                for comment in parsed_comments]
+    book_genres = page_html.find('span', class_='d_book').find('a')
+    parsed_book_description = {
+        'title': book_title,
+        'author': book_author,
+        'image_url': book_image_url,
+        'genre': book_genres.text,
+        'comments': comments,
+    }    
+    book_descriptions.append(parsed_book_description)
+    # parsed_book_description_json = json.dumps(
+    #     parsed_book_description,
+    #     ensure_ascii=False
+    #     )
+    # print(parsed_book_description_json)
+    # with open('book_descriptions.json', 'w', encoding='utf8') as json_file:
+    #     json_file.write(parsed_book_description_json)
+    return parsed_book_description, book_descriptions
+
+
+@retry(TimeoutError, ConnectionError,
+       delay=1, backoff=4, max_delay=4)
+def download_book_descriptions(url_template, book_id, script_path, book_descriptions):
+    book_url = url_template.format('txt.php')
+    param = {
+        'id': book_id,
+    }
+    downloading_book_response = requests.get(
+        book_url,
+        verify=False,
+        params=param,
+        allow_redirects=False
+    )
+    check_for_redirect(downloading_book_response)
+    downloading_book_response.raise_for_status()
+    book_text = downloading_book_response.content
+    parsing_url = url_template.format(f'b{book_id}/')
+    parsing_response = requests.get(parsing_url, verify=False)
+    check_for_redirect(parsing_response)
+    parsing_response.raise_for_status()
+    page_html = BeautifulSoup(parsing_response.text, 'lxml')
+    parsed_book_description, book_descriptions = parse_book_page(
+        page_html,
+        parsing_url,
+        book_descriptions
+        )
+    download_txt(book_text, script_path,
+                parsed_book_description['title'])
+    download_image(parsed_book_description['image_url'],
+                script_path)
+    return(book_descriptions)
 
 
 def main():
-    url_template = 'https://tululu.org/'
-    pages_quantity = 10
-    parse_pages(pages_quantity, url_template)
+    script_path = pathlib.Path.cwd()
+    books_path = script_path.joinpath('books')
+    books_path.mkdir(exist_ok=True)
+    images_path = script_path.joinpath('images')
+    images_path.mkdir(exist_ok=True)
+    url_template = 'https://tululu.org/{}'
+    pages_quantity = 4
+    book_ids = parse_pages(pages_quantity)
+    book_descriptions = []
+    for book_id in book_ids:
+        try:
+            book_descriptions = download_book_descriptions(
+                url_template,
+                book_id,
+                script_path,
+                book_descriptions
+                )
+            if book_id == book_ids[-1]:
+                book_descriptions_json = json.dumps(
+                    book_descriptions,
+                    ensure_ascii=False,
+                    indent=4,
+                    sort_keys=True,
+                    )
+                pprint(book_descriptions)
+                with open('book_descriptions.json', 'w', encoding='utf8') as json_file:
+                    json_file.write(book_descriptions_json)
+        except HTTPError as error:
+            logging.error(msg=f'Была обнаружена ошибка {error}')
 
 
 if __name__ == '__main__':
