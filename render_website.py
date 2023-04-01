1199addc6a4479d986810af94391d0053476782d
KamenevArtem <112734642+KamenevArtem@users.noreply.github.com>
2023-04-01 21:42:09 +0300

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -1,14 +1,14 @@
 import json
 import pathlib
 from pathlib import Path
-from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server
 
-def read_json_file():
+
+def render_html():
     script_path = pathlib.Path.cwd()
     books_path = script_path.joinpath('books')
-    books_path.mkdir(exist_ok=True)
     book_descriptions_json_path = Path(books_path).joinpath(
         'book_descriptions.json'
         )
@@ -16,30 +16,23 @@ def read_json_file():
             "r", encoding='utf8') as json_file:
         books_descriptions = json_file.read()
     books = json.loads(books_descriptions)
-    return books
-
-
-def render_html(books_descriptions):
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
     template = env.get_template('template.html')
     rendered_page = template.render(
-        books_descriptions=books_descriptions,
+        books_descriptions=books,
     )
-    print('ddd')
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
-    print('fsdfs')
 
 
 def main():
-    
-    books = read_json_file()
-    render_html(books)
-    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-    server.serve_forever()
+    render_html()
+    server = Server()
+    server.watch('template.html', render_html)
+    server.serve(root='.')
 
 
 if __name__ == '__main__':
