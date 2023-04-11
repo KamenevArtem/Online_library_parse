9931d59d7d82c82516189705db50dd7bd33511d5
KamenevArtem <112734642+KamenevArtem@users.noreply.github.com>
2023-04-11 03:27:12 +0300

    Configure enumerate

--- render_website.py
+++ render_website.py
@@ -31,16 +31,16 @@ def render_html():
         strict=False
         ))
     page_numbers = range(1, len(splitted_books_descriptions)+1)
-    for page_id, books_descriptions_per_page in enumerate(splitted_books_descriptions):
+    for page_id, books_descriptions_per_page in enumerate(splitted_books_descriptions, start=1):
         books_descriptions_in_row = list(chunked(
             books_descriptions_per_page,
             books_quantity_per_row,
             strict=False
             ))
-        template_name = sanitize_filename(f'index{page_id+1}.html')
+        template_name = sanitize_filename(f'index{page_id}.html')
         rendered_page = template.render(
             books_descriptions=books_descriptions_in_row,
-            page_id=page_id+1,
+            page_id=page_id,
             number_of_pages=page_numbers,
         )
         with open(Path(html_pages_path).joinpath(template_name),
